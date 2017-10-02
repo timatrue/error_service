8d4dd94fe71e2369e05fca279212fd3f04795fe9
timatrue <rollmaxidom@gmail.com>
2017-10-02 16:40:51 +0300

    less preprocessor

--- index.cfm
+++ index.cfm
@@ -1,2 +1,3 @@
+<link href="/error_service/styles/main.css" rel="stylesheet" type="text/css"/>
 <cfinclude template="includes/page_menu.cfm" >
 <cfinclude template="includes/login_form.cfm" >
---
+++ less_compile.js
@@ -0,0 +1,13 @@
+const less = require('less');
+const fs   = require('fs');
+const path = require('path');
+
+(function() {
+    const src = "styles/main.less";
+    less.render(fs.readFileSync(src).toString(), {
+        filename: path.resolve(src), // <- here we go
+    }, function(e, output) {
+        console.log(output.css);
+		fs.writeFile('./styles/main.css', output.css)
+    });
+})();
